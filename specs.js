788cd812c5564f26833dee036a87a425fe0a148a
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-10-19 09:50:04 +0200

    ES6

--- specs.js
+++ specs.js
@@ -51,4 +51,55 @@ describe('javaScript', function () {
         }
     });
 
+    it('default params', function() {
+        function greet(greeting = 'Hello World'){
+            console.log(greeting);
+        };
+        greet();
+    });
+
+    it('spread operator', function() {
+        let args = [1,2,3];
+        function test (){
+            console.log(args);
+        }
+        test.apply(null, args);
+        test(...args);
+    });
+
+    
+    it('set', function() {
+        let myArray = [11,22,34,65,34];
+        let mySet = new Set(myArray);
+        
+        mySet.add('100');
+        mySet.add({a: 1, b: 2});
+        
+        console.log(mySet.size);
+        mySet.forEach(function(val){
+            console.log(val);
+        });
+    });
+
+    it('map', function  (){
+        let myMap = new Map([['a1','Hello'], ['b2','Goodbye']]);
+        console.log(myMap);
+    });
+
+    it('prefixer', function  (){
+        function Prefixer (prefix){
+            this.prefix = prefix;
+        }
+        Prefixer.prototype.prefixArray = function  (arr){
+            return arr.map((x) =>{
+                console.log(this.prefix + x);
+            });
+        }
+        let pre = new Prefixer('Hello ');
+        pre.prefixArray(['Brad', 'Jeff']);
+        
+        let add = (a,b) => a+b;
+        console.log(add(2,2));
+    });
+        
 });
